ada640fac26f63ac516cf833abe1a0b773d234b5
Clinton van Ry <clinton.vanry@gmail.com>
2019-01-23 19:24:15 +0000

    initial layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        test one
+        <Layout>
+          <p>test</p>
+        </Layout>
       </div>
     );
   }
