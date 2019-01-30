98d44685cbcc26e8611163662e48b6e8f62eafca
Clinton van Ry <clinton.vanry@gmail.com>
2019-01-30 17:15:53 +0000

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout =(props) => {
     return (
         <>
-        <div>Toolbar, SideDraw, Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
